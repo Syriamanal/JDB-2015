62364e79cbbb100e97f79e1dc63c8c5746c813c6
Syriamanal <shadinachat123@gmail.com>
2015-10-02 10:43:08 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+?>
+<div id="flashcontent">
+<embed type="application/x-shockwave-flash" src="enablecdn/msp_master.swf" style="undefined" id="indx" name="indx" bgcolor="#9ed45c" quality="high" allowscriptaccess="always" base="http://pbskids.org/_/martha/" flashvars="sctn=home&amp;page=homepage&amp;relative=0&amp;root=" height="540" width="1050">
+</div>
